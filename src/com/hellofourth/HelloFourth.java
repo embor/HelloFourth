5328d1b262381b7b0a5d19dd22544af44a3caefd
embor <fangchuangxin@163.com>
2016-08-06 11:36:21 +0800

    jjj

---
+++ src/com/hellofourth/HelloFourth.java
@@ -0,0 +1,10 @@
+package com.hellofourth;
+
+public class HelloFourth {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Fourth: GitHub");
+	}
+
+}
